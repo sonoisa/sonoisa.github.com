d6d5173e9c86feaf335732b99f2b708c9f9a4936
Isao Sonobe <sonoisa@gmail.com>
2012-08-11 01:27:40 +0900

    delete unused page

--- ogrekit/Top_files/Top.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20120811
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Top_files/TopMoz.css')
-Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
